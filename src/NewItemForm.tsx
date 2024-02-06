bd778ec43081ca379a942cab044a38100f096245
username <dhavalchaudhari39@gmail.com>
2024-02-06 13:33:59 +0530

    checking

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -10,12 +10,20 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState(' ')
   const inputRef = useFocus()
 
+  // Add element after clicking on enter rather than on click button
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      onAdd(text)
+    }
+  }
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         ref={inputRef}
         value={text}
         onChange={(e) => setText(e.target.value)}
+        onKeyUp={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>

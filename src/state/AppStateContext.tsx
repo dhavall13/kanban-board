958b33ac1bae90456ea9c72a31c832efc93c6978
username <dhavalchaudhari39@gmail.com>
2024-02-12 14:42:36 +0530

    added statemanagement

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -1,28 +1,17 @@
-import { createContext, useContext, FC, ReactNode } from 'react'
+import { createContext, useContext, FC, ReactNode, Dispatch } from 'react'
+import { AppState, List, Task, appStateReducer } from './appStateReducer'
+import { Action } from './action'
+import { useImmerReducer } from 'use-immer'
 
 type AppStateContextProps = {
   lists: List[]
   getTasksByListId(id: string): Task[]
+  dispatch: Dispatch<Action>
 }
 
 const AppStateContext = createContext<AppStateContextProps>(
   {} as AppStateContextProps
 )
-
-type Task = {
-  id: string
-  text: string
-}
-
-type List = {
-  id: string
-  text: string
-  tasks: Task[]
-}
-
-export type AppState = {
-  lists: List[]
-}
 
 const appData: AppState = {
   lists: [
@@ -45,14 +34,15 @@ const appData: AppState = {
 }
 
 export const AppStateProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const { lists } = appData
+  const [state, dispatch] = useImmerReducer(appStateReducer, appData)
+  const { lists } = state
 
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || []
   }
 
   return (
-    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+    <AppStateContext.Provider value={{ lists, getTasksByListId, dispatch }}>
       {children}
     </AppStateContext.Provider>
   )
